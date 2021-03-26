b35fdb8287fef9a4eb1097979f319ada5c81157b
Xirtid Nepla <avadend.piroserpen@gmail.com>
2021-03-26 17:54:51 +0300

    add narrowed down imports

--- src/index-no-error.ts
+++ src/index-no-error.ts
@@ -1,4 +1,8 @@
-import 'rxjs';
+/* add any single line of the following to fix the compilation error */
+// import 'rxjs';
+// import 'rxjs/internal-compatibility';
+import 'rxjs/internal/symbol/observable';
+
 import { webSocket as rxSocket } from 'rxjs/webSocket'
 
 export default (url, WebSocketCtor) => rxSocket({ url, WebSocketCtor })
